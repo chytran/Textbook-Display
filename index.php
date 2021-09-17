7d4b1caaf53b32ffd9a595455f1450008c451721
chytran <chytran@users.noreply.github.com>
2021-09-17 11:08:58 -0700

    Set up

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project</title>
+
+    <<!--=============== CSS =================-->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+
+</head>
+<body>
+    <?php foreach ($products as $product) { ?>
+        <tr>
+            <td><?php echo $produc;?></td>
+            <td><?php echo "hi";?></td>
+            <td><?php echo "hi";?></td>
+        </tr>  
+    <?php } ?>
+</body>
+</html>

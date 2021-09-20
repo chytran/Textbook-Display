a0edc4a23c1d02e01aebae93979539b2070e566b
chytran <chytran@users.noreply.github.com>
2021-09-20 11:15:24 -0700

    Setting up database to get information

--- database.php
+++ database.php
@@ -1,18 +1,4 @@
-<?php
 
-$dsn = 'mysql:host=localhost:8111;dbname=BookCatalog';
-$username = 'chytran';
-$password = 'nYMnpgWyUzvfR29S';
 
-try {
-    $db = new PDO($dsn, $username, $password);
-    foreach($db->query('SELECT * FROM author') as $row) {
-        print_r($row);
-    }
-    $db = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
 
 

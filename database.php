c9220f8bae38733f8040b887d03b95d295ef631f
chytran <chytran@users.noreply.github.com>
2021-09-23 13:14:49 -0700

    Setting up sort pages individually

--- database.php
+++ database.php
@@ -7,12 +7,14 @@
     // creates a PDO database connection object
     $db = new PDO($dsn, $username, $password); 
 
-    $query = 'SELECT course.courseTitle, course.credit, book.isbn13, book.price, book.bookTitle, coursebook.course, coursebook.book
+    $query = 'SELECT course.courseTitle, course.credit, book.isbn13, book.price, book.bookTitle, course.courseID, coursebook.book
                 FROM course 
                 INNER JOIN courseBook  
                 on course.courseID = coursebook.course
                 INNER JOIN book
-                on book.isbn13 = coursebook.book';
+                on book.isbn13 = coursebook.book
+                ORDER BY course.courseID
+                LIMIT 5';
     $statement = $db->prepare($query);
     // $author_id = 1;
     // $statement->bindValue(':author_id', $author_id);
--- index.php
+++ index.php
@@ -7,20 +7,20 @@
         ?>
         <table border=“1”>
             <tr style="background-color: #99CCFF;">
-                <td style="text-decoration=underline;">Course #</td>
+                <td style="text-decoration=underline;"><a href="priceSort/page1.php">Course #</a></td>
                 <td>Course title</td>
                 <td>Book Image</td>
                 <td>Book Title</td>
-                <td style="text-decoration=underline;">Price</td>
+                <td style="text-decoration=underline;"><a href="priceSort/page1.php">Course #</a></td>
             </tr>
             <?php foreach ($products as $product) { ?>
             <tr>
                 
                 <!-- course #, course title, book image, book title, price -->
-                <td><?php echo $product['course'] . ' (' . $product['credit'] . ')'; ?></td>
+                <td><?php echo $product['courseID'] . ' (' . $product['credit'] . ')'; ?></td>
                 <td><?php echo $product['courseTitle']; ?></td>
-                <td>
-                    <?php if ($product['course'][15] == 'IS 424'){ ?>
+                <td> 
+                    <?php if ($product['courseID'] == 'IS 424'){ ?>
                         <img src="images/<?php echo '9781890774448' . '.jpg'; ?>" alt=""><a href=""></a>
                         <img src="images/<?php echo '9781890774561' . '.jpg'; ?>" alt=""><a href=""></a>
                     <?php } else { ?>
@@ -30,10 +30,27 @@
                 
                 </td>
                 <td><?php echo $product['bookTitle']; ?></td>
-                <td><?php echo '$' . $product['price']; ?></td>
+                <td>
+                    <?php if ($product['courseID'] == 'IS 424'){ ?>
+                        <div>
+                            $33.51
+                            <br>
+                            $34.34
+                        </div>
+                    <?php } else { ?>
+                        <?php echo '$' . $product['price']; ?>
+                    <?php } ?>
+                </td>
             </tr>
             <?php } ?>
         </table>
+        <div class="number__container">
+            <a href="index.php">1</a>
+            <a href="page2.php">2</a>
+            <a href="page3.php">3</a>
+            <a href="page4.php">4</a>
+            <a href="page5.php">5</a>
+        </div>
 <?php
     include_once 'footer.php';
 ?>

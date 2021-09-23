3a6d20ec7002357c64081657d9dc96b310de00cc
chytran <chytran@users.noreply.github.com>
2021-09-23 13:23:38 -0700

    organizing all sort pages

--- courseSort/page1.php
+++ courseSort/page1.php
@@ -1,10 +1,32 @@
 <?php
     include_once 'header.php';
 ?>
-        <?php
-            include_once 'database.php' 
+<?php
+    $dsn = 'mysql:host=localhost:8111;dbname=BookCatalog';
+    // $dsn = 'mysql:host=localhost;dbname=BookCatalog';
+    $username = 'mgs_user';
+    $password = 'pa55word';
 
-        ?>
+    // creates a PDO database connection object
+    $db = new PDO($dsn, $username, $password); 
+
+    $query = 'SELECT course.courseTitle, course.credit, book.isbn13, book.price, book.bookTitle, course.courseID, coursebook.book
+                FROM course 
+                INNER JOIN courseBook  
+                on course.courseID = coursebook.course
+                INNER JOIN book
+                on book.isbn13 = coursebook.book
+                ORDER BY course.courseID
+                LIMIT 5';
+    $statement = $db->prepare($query);
+    // $author_id = 1;
+    // $statement->bindValue(':author_id', $author_id);
+    $statement->execute();
+
+    // fetchAll() gets all rows from the prepared query statement
+    $products = $statement->fetchAll();
+    $statement->closeCursor();
+    ?>
         <table border=“1”>
             <tr style="background-color: #99CCFF;">
                 <td style="text-decoration=underline;"><a href="courseSort/page1.php">Course #</a></td>

c6fa546b90455fb006df4e4814126985583e7abb
chytran <chytran@users.noreply.github.com>
2021-09-23 22:58:50 -0700

    Trying to figure out display image value

--- detailed.php
+++ detailed.php
@@ -15,32 +15,36 @@
     $db = new PDO($dsn, $username, $password); 
 
     // Get image result from submission
-    // $imageValue = filter_input(INPUT_POST, 'imageValue');
-    $imageValue = $_GET['imageValue'];
+    $imageValue = filter_input(INPUT_POST, 'imageValue');
+    // $imageValue = $_GET['imageValue'];
 
-    $query = 'SELECT course.courseID, course.courseTitle, course.credit, 
-                book.bookTitle, 
-                book.price, 
-                authorbook.author = :author_id, authorbook.book
-                book.publisher,
-                book.edition,
-                book.length,
-                book.isbn13, 
-                book.description
-                FROM course 
-                INNER JOIN authorbook  
-                on authorbook.book = book.isbn13
-                INNER JOIN book
-                on book.publisher = authorbook.author
-                LIMIT 1';
-    $statement = $db->prepare($query);
-    $author_id = 1;
-    $statement->bindValue(':author_id', $author_id);
-    $statement->execute();
+    // $query = 'SELECT course.courseID, course.courseTitle, course.credit, 
+    //             book.bookTitle, 
+    //             book.price, 
+    //             authorbook.author, authorbook.book,
+    //             book.publisher,
+    //             book.isbn13
+    //             book.edition,
+    //             book.length, 
+    //             book.description
+    //             FROM course 
+    //             INNER JOIN authorbook  
+    //             on authorbook.book = book.isbn13
+    //             INNER JOIN book
+    //             on book.publisher = authorbook.author
+    //             LIMIT 1';
 
-    // fetchAll() gets all rows from the prepared query statement
-    $products = $statement->fetchAll();
-    $statement->closeCursor();
+    $query = 'SELECT course, book
+                FROM coursebook
+                WHERE book = :book';
+                $statement = $db->prepare($query);
+                $coursebook_book = $imageValue;
+                $statement->bindValue(':book', $coursebook_book);
+                $statement->execute();
+
+                // fetchAll() gets all rows from the prepared query statement
+                $products = $statement->fetchAll();
+                $statement->closeCursor();
     ?>
 
     <!-- courseID Course Title Credits -->
@@ -55,14 +59,24 @@
     <!-- Production Description -->
     SELECT * FROM 
 
+    <p>
+        <?php echo $imageValue; ?>
+    </p>
     <table border=“1”>
         <tr>
             <td>
-                
+                <img src="images/<?php echo $imageValue;?>" alt="">
             </td>
-            <td></td>
+            <td>
+                <div style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+                    <p>For course: <?php echo $imageValue;?></p>
+                    
+                </div>
+            </td>
         </tr>
-        <tr></tr>
+        <tr>
+
+        </tr>
     </table>
 
 <!-- Footer -->

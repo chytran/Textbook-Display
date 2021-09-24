51cb357611f73519197ef21aa94f2fa99a736c11
chytran <chytran@users.noreply.github.com>
2021-09-24 11:58:25 -0700

    form component to skip time

---
+++ form.php
@@ -0,0 +1,13 @@
+<form action="detailed.php" method="POST">
+    <input type="hidden" name="courseTitle" value="<?php echo $product['courseTitle']; ?>">
+    <input type="hidden" name="bookTitle" value="<?php echo $product['bookTitle']; ?>">
+    <input type="hidden" name="credit" value="<?php echo $product['credit']; ?>">
+    <input type="hidden" name="price" value="<?php echo $product['price']; ?>">
+    <input type="hidden" name="author" value="<?php echo $product['firstName'] . ' ' . $product['lastName']; ?>">
+    <input type="hidden" name="publisher" value="<?php echo $product['publisher']; ?>">
+    <input type="hidden" name="edition" value="<?php echo $product['edition']; ?>">
+    <input type="hidden" name="publishDate" value="<?php echo $product['publishDate']; ?>">
+    <input type="hidden" name="length" value="<?php echo $product['length']; ?>">
+    <input type="hidden" name="imageValue" value="<?php echo $product['book'];?>">
+    <input type="image" src="images/<?php echo $product['book'] . '.jpg'; ?>">
+</form>
--- index.php
+++ index.php
@@ -24,19 +24,9 @@
                         <img src="images/<?php echo '9781890774448' . '.jpg'; ?>" alt=""><a href=""></a>
                         <img src="images/<?php echo '9781890774561' . '.jpg'; ?>" alt=""><a href=""></a>
                     <?php } else { ?>
-                        <form action="detailed.php" method="POST">
-                        <input type="hidden" name="courseTitle" value="<?php echo $product['courseTitle']; ?>">
-                            <input type="hidden" name="bookTitle" value="<?php echo $product['bookTitle']; ?>">
-                            <input type="hidden" name="credit" value="<?php echo $product['credit']; ?>">
-                            <input type="hidden" name="price" value="<?php echo $product['price']; ?>">
-                            <input type="hidden" name="author" value="<?php echo $product['firstName'] . ' ' . $product['lastName']; ?>">
-                            <input type="hidden" name="publisher" value="<?php echo $product['publisher']; ?>">
-                            <input type="hidden" name="edition" value="<?php echo $product['edition']; ?>">
-                            <input type="hidden" name="publishDate" value="<?php echo $product['publishDate']; ?>">
-                            <input type="hidden" name="length" value="<?php echo $product['length']; ?>">
-                            <input type="hidden" name="imageValue" value="<?php echo $product['book'];?>">
-                            <input type="image" src="images/<?php echo $product['book'] . '.jpg'; ?>">
-                        </form>
+                        <?php
+                            include 'form.php';
+                        ?>
                     <?php } ?>
 
                 
@@ -66,19 +56,9 @@
                         <img src="images/<?php echo '9781890774448' . '.jpg'; ?>" alt=""><a href=""></a>
                         <img src="images/<?php echo '9781890774561' . '.jpg'; ?>" alt=""><a href=""></a>
                     <?php } else { ?>
-                        <form action="detailed.php" method="POST">
-                            <input type="hidden" name="courseTitle" value="<?php echo $product['courseTitle']; ?>">
-                            <input type="hidden" name="bookTitle" value="<?php echo $product['bookTitle']; ?>">
-                            <input type="hidden" name="credit" value="<?php echo $product['credit']; ?>">
-                            <input type="hidden" name="price" value="<?php echo $product['price']; ?>">
-                            <input type="hidden" name="author" value="<?php echo $product['firstName'] . ' ' . $product['lastName']; ?>">
-                            <input type="hidden" name="publisher" value="<?php echo $product['publisher']; ?>">
-                            <input type="hidden" name="edition" value="<?php echo $product['edition']; ?>">
-                            <input type="hidden" name="publishDate" value="<?php echo $product['publishDate']; ?>">
-                            <input type="hidden" name="length" value="<?php echo $product['length']; ?>">
-                            <input type="hidden" name="imageValue" value="<?php echo $product['book'];?>">
-                            <input type="image" src="images/<?php echo $product['book'] . '.jpg'; ?>">
-                        </form>
+                        <?php
+                            include 'form.php';
+                        ?>
                     <?php } ?>
 
                 

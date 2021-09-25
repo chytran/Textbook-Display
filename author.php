151eb87ae0d85f2d594045dba20d03f39e70966d
chytran <chytran@users.noreply.github.com>
2021-09-25 11:57:36 -0700

    Author completed

--- author.php
+++ author.php
@@ -1,36 +1,36 @@
 <?php
     if ($imageValue == '9781890774561') { // CLEAR
         $author = 'Joel Murach, Ray Harris'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
     } else if ($imageValue == '9781890774448') { // CLEAR
         $author = 'Joel Murach, Andrea Steelman'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
     } else if ($imageValue == '9781890774424') { // CLEAR
         $author = 'Joel Murach, Andrea Steelman, Doug Lowe'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      }  else if ($imageValue == '9781420065503') { // CLAR
         $author = 'Frederick Gallegos, Sandra Senft'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      }  else if ($imageValue == '9780735627406') { // CLEAR
         $author = 'Tony Northrup, Mike Snell'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      }  else if ($imageValue == '9780672321115') { //CLEAR
         $author = 'Kenn Scribner, Mark Stiver'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      }  else if ($imageValue == '9780596527525') { // CLEAR
         $author = 'Jennifer Robbins, Aaron Gustafson'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      } else if ($imageValue == '9780470178454') { // CLEAR
         $author = 'Len Silverston, Paul Agnew'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      } else if ($imageValue == '9780470074787') { // CLEAR
         $author = 'Alan Dennis, Barbara Wixom'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
      } else if ($imageValue == '9780136088394') { // CLEAR
         $author = 'Ramesh Venkataraman, Heikki Topi'; // CLEAR
-        echo $author;
+        echo "<p>" . "Authors: " . $author . "</p>";
     } else { 
-        echo "<p>" . "Authors:" . $author; "</p>";
+        echo "<p>" . "Authors: " . $author . "</p>";
     } 
 
 ?>

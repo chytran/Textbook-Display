708b20c0192692e6b24ba29184edac88c0d5f222
chytran <chytran@users.noreply.github.com>
2021-09-24 18:44:05 -0700

    Page 2 and 3 completed unsorted

--- page3.php
+++ page3.php
@@ -1,5 +1,5 @@
 <?php
-    include_once '../header.php';
+    include_once 'header.php';
 ?>
 <?php
     $dsn = 'mysql:host=localhost:8111;dbname=BookCatalog';
@@ -10,14 +10,24 @@
     // creates a PDO database connection object
     $db = new PDO($dsn, $username, $password); 
 
-    $query = 'SELECT course.courseTitle, course.credit, book.isbn13, book.price, book.bookTitle, course.courseID, coursebook.book
-                FROM course 
-                INNER JOIN courseBook  
-                on course.courseID = coursebook.course
-                INNER JOIN book
-                on book.isbn13 = coursebook.book
-                ORDER BY course.courseID
-                LIMIT 5 OFFSET 10';
+    $query = 'SELECT course.courseTitle, course.credit, course.courseID,
+    book.isbn13, book.price, book.bookTitle, book.publisher, book.edition, book.length, book.description, book.publishDate,
+    coursebook.book,
+    publisher.publisherID, publisher.publisher,
+    authorbook.author, authorbook.book,
+    author.authorID, author.firstName, author.lastName
+    FROM course 
+    INNER JOIN coursebook  
+    on coursebook.course = course.courseID 
+    INNER JOIN book
+    on book.isbn13 = coursebook.book
+    INNER JOIN publisher
+    on book.publisher = publisher.publisherID  
+    INNER JOIN authorbook
+    on authorbook.book = book.isbn13 
+    INNER JOIN author
+    on author.authorID = authorbook.author
+    LIMIT 2 OFFSET 19';
     $statement = $db->prepare($query);
     // $author_id = 1;
     // $statement->bindValue(':author_id', $author_id);
@@ -26,14 +36,44 @@
     // fetchAll() gets all rows from the prepared query statement
     $products = $statement->fetchAll();
     $statement->closeCursor();
+
+    // Query 2
+    $query2 = 'SELECT course.courseTitle, course.credit, course.courseID,
+    book.isbn13, book.price, book.bookTitle, book.publisher, book.edition, book.length, book.description, book.publishDate,
+    coursebook.book,
+    publisher.publisherID, publisher.publisher,
+    authorbook.author, authorbook.book,
+    author.authorID, author.firstName, author.lastName
+    FROM course 
+    INNER JOIN coursebook  
+    on coursebook.course = course.courseID 
+    INNER JOIN book
+    on book.isbn13 = coursebook.book
+    INNER JOIN publisher
+    on book.publisher = publisher.publisherID  
+    INNER JOIN authorbook
+    on authorbook.book = book.isbn13 
+    INNER JOIN author
+    on author.authorID = authorbook.author
+    LIMIT 3 OFFSET 22';
+    $statement = $db->prepare($query2);
+    // $author_id = 1;
+    // $statement->bindValue(':author_id', $author_id);
+    $statement->execute();
+
+    // fetchAll() gets all rows from the prepared query statement
+    $products2 = $statement->fetchAll();
+    $statement->closeCursor();
+
+    // Query 2
     ?>
         <table border=“1”>
             <tr style="background-color: #99CCFF;">
-                <td style="text-decoration=underline;"><a href="page1.php">Course #</a></td>
+                <td style="text-decoration=underline;"><a href="courseSort.page1.php">Course #</a></td>
                 <td>Course title</td>
                 <td>Book Image</td>
                 <td>Book Title</td>
-                <td style="text-decoration=underline;"><a href="../priceSort/page1.php">Price</a></td>
+                <td style="text-decoration=underline;"><a href="priceSort/page1.php">Price</a></td>
             </tr>
             <?php foreach ($products as $product) { ?>
             <tr>
@@ -46,7 +86,9 @@
                         <img src="..images/<?php echo '9781890774448' . '.jpg'; ?>" alt=""><a href=""></a>
                         <img src="..images/<?php echo '9781890774561' . '.jpg'; ?>" alt=""><a href=""></a>
                     <?php } else { ?>
-                        <img src="..images/<?php echo $product['isbn13'] . '.jpg'; ?>" alt=""><a href=""></a>
+                        <?php
+                            include 'formMain.php';
+                        ?>
                     <?php } ?>
 
                 
@@ -65,14 +107,50 @@
                 </td>
             </tr>
             <?php } ?>
+
+            <!-- 2nd -->
+            <?php foreach ($products2 as $product) { ?>
+            <tr>
+                
+                <!-- course #, course title, book image, book title, price -->
+                <td><?php echo $product['courseID'] . ' (' . $product['credit'] . ')'; ?></td>
+                <td><?php echo $product['courseTitle']; ?></td>
+                <td> 
+                    <?php if ($product['courseID'] == 'IS 424'){ ?>
+                        <img src="images/<?php echo '9781890774448' . '.jpg'; ?>" alt=""><a href=""></a>
+                        <img src="images/<?php echo '9781890774561' . '.jpg'; ?>" alt=""><a href=""></a>
+                    <?php } else { ?>
+                        <?php
+                            include 'formMain.php';
+                        ?>
+                    <?php } ?>
+
+                
+                </td>
+                <td><?php echo $product['bookTitle']; ?></td>
+                <td>
+                    <?php if ($product['courseID'] == 'IS 424'){ ?>
+                        <div>
+                            $33.51
+                            <br>
+                            $34.34
+                        </div>
+                    <?php } else { ?>
+                        <?php echo '$' . $product['price']; ?>
+                    <?php } ?>
+                </td>
+            </tr>
+            <?php } ?>
+
+            
         </table>
         <div class="number__container">
-            <a href="page1.php">1</a>
+            <a href="index.php">1</a>
             <a href="page2.php">2</a>
             <a href="page3.php">3</a>
             <a href="page4.php">4</a>
             <a href="page5.php">5</a>
         </div>
 <?php
-    include_once '../footer.php';
+    include_once 'footer.php';
 ?>
